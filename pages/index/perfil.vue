1b9e2dac4fac828a33b63e90cb0c475fcebb2024
t0nyle0n <anthonyfleon@hotmail.com>
2019-11-11 00:46:09 -0500

    link_inicio

--- pages/index/perfil.vue
+++ pages/index/perfil.vue
@@ -1,102 +1,120 @@
 <template>
-  <form @submit.prevent="submit">
-    <div class="mb-4 flex flex-wrap items-center">
-      <label for="first-name" class="block md:px-2 mb-1 md:w-2/5">
-        Nombres
-      </label>
-      <input
-        id="first-name"
-        v-model.trim="userdate.firstName"
-        class="form-input md:w-3/5"
-        type="text"
-        pattern="[a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜñÑ ]{2,}"
-        title="Solo letras"
-        required
-      />
-    </div>
-    <div class="mb-4 flex flex-wrap items-center">
-      <label for="last-name" class="block md:px-2 mb-1 md:w-2/5">
-        Apellidos
-      </label>
-      <input
-        id="last-name"
-        v-model.trim="userdate.lastName"
-        class="form-input md:w-3/5"
-        type="text"
-        pattern="[a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜñÑ ]{2,}"
-        title="Solo letras"
-        required
-      />
-    </div>
-    <div class="mb-4 flex flex-wrap items-center">
-      <label for="birthdate" class="block md:px-2 mb-1 md:w-2/5">
-        Fecha de nacimiento
-      </label>
-      <div class="flex md:w-3/5 -mx-1">
-        <div class="w-1/3 px-1">
-          <input
-            id="birthdate"
-            v-model="fecha.year"
-            type="number"
-            class="form-input text-center"
-            placeholder="Año"
-            :min="year - 100"
-            :max="year - 18"
-            required
-          />
-        </div>
-        <div class="w-1/3 px-1 ">
-          <input
-            id="month"
-            v-model="fecha.month"
-            type="number"
-            class="form-input text-center"
-            placeholder="Mes"
-            min="1"
-            max="12"
-            required
-          />
-        </div>
-        <div class="w-1/3 px-1 ">
-          <input
-            id="day"
-            v-model="fecha.day"
-            type="number"
-            class="form-input text-center"
-            placeholder="Día"
-            min="1"
-            :max="lastDay"
-            required
-          />
+  <div>
+    <form v-if="!estado" @submit.prevent="submit">
+      <div class="mb-4 flex flex-wrap items-center">
+        <label for="first-name" class="block md:px-2 mb-1 md:w-2/5">
+          Nombres
+        </label>
+        <input
+          id="first-name"
+          v-model.trim="userdate.firstName"
+          class="form-input md:w-3/5"
+          type="text"
+          pattern="[a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜñÑ ]{2,}"
+          title="Solo letras"
+          required
+        />
+      </div>
+      <div class="mb-4 flex flex-wrap items-center">
+        <label for="last-name" class="block md:px-2 mb-1 md:w-2/5">
+          Apellidos
+        </label>
+        <input
+          id="last-name"
+          v-model.trim="userdate.lastName"
+          class="form-input md:w-3/5"
+          type="text"
+          pattern="[a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜñÑ ]{2,}"
+          title="Solo letras"
+          required
+        />
+      </div>
+      <div class="mb-4 flex flex-wrap items-center">
+        <label for="birthdate" class="block md:px-2 mb-1 md:w-2/5">
+          Fecha de nacimiento
+        </label>
+        <div class="flex md:w-3/5 ">
+          <div class="w-1/3 px-1">
+            <input
+              id="birthdate"
+              v-model="fecha.year"
+              type="number"
+              class="form-input text-center"
+              placeholder="Año"
+              :min="year - 100"
+              :max="year - 18"
+              required
+            />
+          </div>
+          <div class="w-1/3 px-1 ">
+            <input
+              id="month"
+              v-model="fecha.month"
+              type="number"
+              class="form-input text-center"
+              placeholder="Mes"
+              min="1"
+              max="12"
+              required
+            />
+          </div>
+          <div class="w-1/3 px-1 ">
+            <input
+              id="day"
+              v-model="fecha.day"
+              type="number"
+              class="form-input text-center"
+              placeholder="Día"
+              min="1"
+              :max="lastDay"
+              required
+            />
+          </div>
         </div>
       </div>
-    </div>
-    <div class="mb-4 flex flex-wrap items-center">
-      <label for="country" class="block md:px-2 mb-1 md:w-2/5">
-        País
-      </label>
-      <select
-        id="country"
-        v-model="userdate.country"
-        class="form-input md:w-3/5"
-        required
+      <div class="mb-4 flex flex-wrap items-center">
+        <label for="country" class="block md:px-2 mb-1 md:w-2/5">
+          País
+        </label>
+        <select
+          id="country"
+          v-model="userdate.country"
+          class="form-input md:w-3/5"
+          required
+        >
+          <option value selected disabled>
+            Selecciona tu país
+          </option>
+          <option
+            v-for="c in countries"
+            :key="c"
+            class="text-center"
+            :value="c"
+          >
+            {{ c }}
+          </option>
+        </select>
+      </div>
+      <div class="mb-4">
+        <button
+          class="w-full p-2 bg-primary text-white rounded focus:outline-none focus:shadow-outline "
+        >
+          Actualizar Datos
+        </button>
+      </div>
+    </form>
+    <div v-if="estado" class="text-center text-grey-700">
+      <h3 class="text-xl mb-6 md:w-2/3 mx-auto">
+        ¡Se Actualizaron los datos !
+      </h3>
+      <nuxt-link
+        to="/"
+        class="w-full inline-block sm:w-auto border-2 border-white hover:bg-white hover:text-primary py-3 px-6 m-2 rounded-lg text-xl"
       >
-        <option value selected disabled>
-          Selecciona tu país
-        </option>
-        <option v-for="c in countries" :key="c" class="text-center" :value="c">
-          {{ c }}
-        </option>
-      </select>
+        Volver a Inicio
+      </nuxt-link>
     </div>
-    <div class="mb-4">
-      <button
-        class="w-full p-2 bg-primary text-white rounded focus:outline-none focus:shadow-outline "
-      >
-        Actualizar Datos
-      </button>
-    </div>
-  </form>
+  </div>
 </template>
 
 <script>
@@ -104,6 +122,7 @@ import { mapState } from 'vuex'
 export default {
   data() {
     return {
+      estado: false,
       countries: [],
       userdate: [],
       fecha: {
@@ -143,6 +162,9 @@ export default {
     this.fecha.day = await this.fechaArreglo[2]
   },
   methods: {
+    goHome() {
+      return this.$auth.redirect('/')
+    },
     async submit() {
       try {
         await this.$axios.$put('/perfil', {
@@ -152,11 +174,12 @@ export default {
         this.$emit('done', {
           email: this.userdate.email
         })
+        this.estado = !this.estado
       } catch (e) {
         if (e.response && e.response.status === 400) {
           const { message } = e.response.data
           this.$notify({
-            group: 'auth',
+            group: 'main',
             type: 'error',
             title: 'Error',
             text: message
